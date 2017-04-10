2b67fa71b483d527ec406cbb4daccff92daa7409
tbone255 <taha.amir13971@gmail.com>
2017-04-10 16:32:17 -0400

    now you can ctrl+c to quit server (osx compatible)

--- sqlite_testing_for_front_end/sqliteTestServer.py
+++ sqlite_testing_for_front_end/sqliteTestServer.py
@@ -4,6 +4,10 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
+import logging
+import signal
+
+is_closing = False
 
 conn = sqlite3.connect('sqlitetest.db') #database file
 conn.row_factory = sqlite3.Row #row object
@@ -13,7 +17,11 @@ c.execute(' SELECT * FROM EventLog ')
 l = c.fetchall()
 rows = [dict(i) for i in l]
 
-print rows
+for r in rows:
+	print '----------------------------------------------------'
+	print r
+
+print '----------------------------------------------------'
 
 
 
@@ -27,8 +35,27 @@ def make_app():
 		(r'/(tomato.png)', tornado.web.StaticFileHandler, {'path':'./'}),
 	])
 
-app = make_app()
-app.listen(8888)
-tornado.ioloop.IOLoop.current().start()
 
-conn.close()
+
+
+
+
+def signal_handler(signum, frame):
+	global is_closing
+	logging.info('exiting, please wait')
+	is_closing = True
+
+def exit():
+	global is_closing
+	if(is_closing):
+		tornado.ioloop.IOLoop.instance().stop()
+		conn.close()
+		logging.info('exited')
+
+if __name__ == "__main__":
+	tornado.options.parse_command_line()
+	signal.signal(signal.SIGINT, signal_handler)
+	app = make_app()
+	app.listen(8888)
+	tornado.ioloop.PeriodicCallback(exit, 100).start()
+	tornado.ioloop.IOLoop.current().start()
