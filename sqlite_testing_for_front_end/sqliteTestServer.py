0322c9cf8e62338a282b908ce119353ec361b710
tbone255 <taha.amir13971@gmail.com>
2017-04-12 18:33:02 -0400

    'add'

--- sqlite_testing_for_front_end/sqliteTestServer.py
+++ sqlite_testing_for_front_end/sqliteTestServer.py
@@ -6,25 +6,36 @@ import tornado.options
 import tornado.web
 import logging
 import signal
+import time
 
 is_closing = False
+
 
 conn = sqlite3.connect('sqlitetest.db') #database file
 conn.row_factory = sqlite3.Row #row object
 c = conn.cursor()
 
-c.execute(' SELECT * FROM EventLog ')
-l = c.fetchall()
-rows = [dict(i) for i in l]
-
 class MainHandler(tornado.web.RequestHandler):
 	def get(self):
+		c.execute(' SELECT * FROM EventLog ')
+		l = c.fetchall()
+		rows = [dict(i) for i in l]
 		self.render("index.html", entries = rows)
 
 
 class AddHandler(tornado.web.RequestHandler):
 	def get(self):
-		self.render("add.html")
+		self.render('add.html')
+
+	def post(self):
+		event = self.get_argument(name='event')
+		veg = self.get_argument(name='veg')
+		planter = int(self.get_argument(name='planter'))
+		print type(planter)
+		timelog = time.strftime('%Y-%m-%d %H:%M:%S')
+		c.execute("INSERT INTO EventLog (Event, Vegetable, Timelog, Planter) VALUES (\'%s\',\'%s\',\'%s\',%d);" % (event, veg, timelog, planter))
+		conn.commit()
+		self.redirect("/add")
 def make_app():
 	return tornado.web.Application([
 		(r'/', MainHandler),
@@ -32,9 +43,6 @@ def make_app():
 		(r'/(custom.css)', tornado.web.StaticFileHandler, {'path': './'}),
 		(r'/add', AddHandler)
 	])
-
-
-
 
 
 #looks for a signal
