c2704bec004a6a09bd9323273c878afbb500cb1d
eli <elixircist@gmail.com>
2017-04-20 12:46:48 -0400

    added all tables to make sql, added bogus/interesting info to tables

--- harvester/sql/makesql.py
+++ harvester/sql/makesql.py
@@ -1,7 +1,7 @@
 import MySQLdb
 
 #create a connection to MySQL
-connection = MySQLdb.connect(host = "localhost", user = "root", passwd='toor', db = "harvester"	 )
+connection = MySQLdb.connect(host = "localhost", user = "root", db = "harvester"	 )
 #create cursor object whcih lets us interact with the database
 cursor = connection.cursor()
 
@@ -10,38 +10,51 @@ cursor = connection.cursor()
 #create table for tracking plant events
 cursor.execute('''
 
-	CREATE TABLE `plant` (
-		`plant_id`			INTEGER			NOT NULL AUTO_INCREMENT,
-		`type`				VARCHAR(32)		NOT NULL,
-		`scientific_name`	VARCHAR(128)	NOT NULL,
-		`description`		VARCHAR(512)	NOT NULL,
-		PRIMARY KEY (`plant_id`)
-	) ENGINE=InnoDB;
+		CREATE TABLE `plant` (
+		    `plant_id`          INTEGER      NOT NULL 		AUTO_INCREMENT,
+		    `type`              VARCHAR(32)  NOT NULL,
+		    `scientific_name`   VARCHAR(128) NOT NULL,
+		    `description`       VARCHAR(512) NOT NULL,
+		    PRIMARY KEY (`plant_id`)
+		) ENGINE=InnoDB;
 
-	CREATE TABLE `event` (
-		`event_id`			INTEGER			NOT NULL AUTO_INCREMENT,
-		`event_type`		VARCHAR(32)		NOT NULL,
-		PRIMARY KEY (`event_id`)
-	) ENGINE=InnoDB;
+		CREATE TABLE `planter` (
+		    `planter_id`        INTEGER     NOT NULL 		AUTO_INCREMENT,
+		    `x_coordinate`      INTEGER     DEFAULT NULL,
+		    `y_coordinate`      INTEGER     DEFAULT NULL,
+		    `length`            INTEGER     NOT NULL,
+		    `width`             INTEGER     NOT NULL,
+		    PRIMARY KEY (`planter_id`)
+		) ENGINE=InnoDB;
 
-	CREATE TABLE `nutrition` (
-		`nutrition_id`		INTEGER			NOT NULL AUTO_INCREMENT,
-		`brand`				VARCHAR(32)		DEFAULT NULL,
-		`type`				VARCHAR(32)		NOT NULL,
-		`description`		VARCHAR(128)	NOT NULL,
-		PRIMARY KEY (`nutrition_id`)
-	) ENGINE=InnoDB;
+		CREATE TABLE `event` (
+		    `event_id`          INTEGER      NOT NULL 		AUTO_INCREMENT,
+		    `event_type`        VARCHAR(32)  NOT NULL,
+		    `description`       VARCHAR(128) NOT NULL,
+		    PRIMARY KEY (`event_id`)
+		) ENGINE=InnoDB;
 
-	CREATE TABLE `event_log` (
-		`event_log_id`		INTEGER			NOT NULL AUTO_INCREMENT,
-		`event_id`			INTEGER			NOT NULL,
-		`plant_id`			INTEGER,
-		`planter_id`		INTEGER,
-		`nutrition_id`		INTEGER			DEFAULT NULL,
-		`timestamp`			DATETIME		DEFAULT NULL,
-		`description`		VARCHAR(256),
-		PRIMARY KEY (`event_log_id`)
-	) ENGINE=InnoDB;
+		CREATE TABLE `nutrition` (
+		    `nutrition_id`      INTEGER      NOT NULL 		AUTO_INCREMENT,
+		    `brand`             VARCHAR(32)  DEFAULT    	NULL,
+		    `type`              VARCHAR(32)  NOT NULL,
+		    `description`       VARCHAR(128) NOT NULL,
+		    PRIMARY KEY (`nutrition_id`)
+		) ENGINE=InnoDB;
+
+		CREATE TABLE `event_log` (
+		    `event_log_id`      INTEGER     NOT NULL AUTO_INCREMENT,
+		    `event_id`          INTEGER     NOT NULL,
+		    `plant_id`          INTEGER,
+		    `planter_id`        INTEGER,
+		    `nutrition_id`      INTEGER     DEFAULT NULL,
+		    `timestamp`         DATETIME    DEFAULT NULL,
+		    `description`       VARCHAR(256),
+		    PRIMARY KEY (`event_log_id`),
+		    FOREIGN KEY (`event_id`) REFERENCES `event` (`event_id`),
+		    FOREIGN KEY (`plant_id`) REFERENCES `plant` (`plant_id`),
+		    FOREIGN KEY (`planter_id`) REFERENCES `planter` (`planter_id`)
+		) ENGINE=InnoDB;
 
 ''')
 
--- harvester/sql/sql_data_gen.py
+++ harvester/sql/sql_data_gen.py
@@ -1,10 +1,96 @@
+import MySQLdb
+
 #create a connection to MySQL
-connection = MySQLdb.connect(host = "localhost", user = "root", passwd='toor', db = "harvester"	 )
+connection = MySQLdb.connect(host = "localhost", user = "root", db = "harvester"	 )
 #create cursor object whcih lets us interact with the database
 cursor = connection.cursor()
 
-cursor.execute('INSERT INTO plant (type, scientific_name, description) VALUES (%s, %s, %s)' %  ('Beef Tomato', 'Solanum lycopersicum', 'a big tomato'))
-cursor.execute('INSERT INTO plant (type, scientific_name, description) VALUES (%s, %s, %s)' %  ('Potato', 'Solanum tuberosum', 'turn into french fries'))
-cursor.execute('INSERT INTO plant (type, scientific_name, description) VALUES (%s, %s, %s)' %  ('Eggplant', 'Solanum melongena', 'for making eggplant food'))
-cursor.execute('INSERT INTO plant (type, scientific_name, description) VALUES (%s, %s, %s)' %  ('Spearmint', 'Mentha', 'for making gum'))
-cursor.execute('INSERT INTO plant (type, scientific_name, description) VALUES (%s, %s, %s)' %  ('Pepper', 'Piper nigrum', 'mexican food'))
+cursor.execute(''' INSERT INTO plant (type, scientific_name, description) VALUES ("%s", "%s", "%s") ''' 
+				%  ("Beef Tomato", "Solanum lycopersicum", "a big tomato"))
+
+cursor.execute(''' INSERT INTO plant (type, scientific_name, description) VALUES ("%s", "%s", "%s") '''
+				%  ('Potato', 'Solanum tuberosum', 'turn into french fries'))
+
+cursor.execute(''' INSERT INTO plant (type, scientific_name, description) VALUES ("%s", "%s", "%s") '''
+				%  ('Eggplant', 'Solanum melongena', 'for making eggplant food'))
+
+cursor.execute(''' INSERT INTO plant (type, scientific_name, description) VALUES ("%s", "%s", "%s") '''
+				%  ('Spearmint', 'Mentha', 'for making gum'))
+
+cursor.execute(''' INSERT INTO plant (type, scientific_name, description) VALUES ("%s", "%s", "%s") '''
+				%  ('Pepper', 'Piper nigrum', 'mexican food'))
+
+
+
+cursor.execute(''' INSERT INTO planter (length, width) VALUES (%s, %s) ''' 
+				%  (60, 48))
+
+cursor.execute(''' INSERT INTO planter (length, width) VALUES (%s, %s) ''' 
+				%  (48, 48))
+
+cursor.execute(''' INSERT INTO planter (length, width) VALUES (%s, %s) ''' 
+				%  (48, 48))
+
+cursor.execute(''' INSERT INTO planter (length, width) VALUES (%s, %s) ''' 
+				%  (60, 60))
+
+
+
+cursor.execute(''' INSERT INTO event (event_type, description) VALUES ("%s", "%s") '''
+				% ("Harvest", "Harvested peppers"))
+
+cursor.execute(''' INSERT INTO event (event_type, description) VALUES ("%s", "%s") '''
+				% ("Water", "Give plants water"))
+
+cursor.execute(''' INSERT INTO event (event_type, description) VALUES ("%s", "%s") '''
+				% ("Feed", "Give plants food"))
+
+cursor.execute(''' INSERT INTO event (event_type, description) VALUES ("%s", "%s") '''
+				% ("Tornado", "Tornado ravaged lands, greenhouse is lost"))
+
+cursor.execute(''' INSERT INTO event (event_type, description) VALUES ("%s", "%s") '''
+				% ("Shit", "Shit in the compost"))
+
+
+cursor.execute(''' INSERT INTO nutrition (brand, type, description) VALUES ("%s", "%s", "%s") '''
+				% ("Peat", "0-11-11", "Good for tomatoes"))
+
+cursor.execute(''' INSERT INTO nutrition (brand, type, description) VALUES ("%s", "%s", "%s") '''
+				% ("Buddy's", "8-8-8", "Crop fertilizer"))
+
+
+
+cursor.execute(''' INSERT INTO event_log (event_id, plant_id, planter_id, timestamp, description) VALUES ("%s", "%s", "%s", "%s", "%s") '''
+				% ("11", "11", "9", "2017-04-01 10:00:00", "harvested 10oz peppers"))
+
+cursor.execute(''' INSERT INTO event_log (event_id, plant_id, planter_id, timestamp, description) VALUES ("%s", "%s", "%s", "%s", "%s") '''
+				% ("12", "12", "10", "2017-04-02 1:30:00", "watered plants"))
+
+#
+#cursor.execute(''' INSERT INTO event_log (event_id, plant_id, planter_id, nutrition_id, tstamp, description) VALUES (%s, %s, %s, %s, %s, %s) '''
+#				% ("13", "13", "11", "", "", ""))
+#
+#cursor.execute(''' INSERT INTO event_log (event_id, plant_id, planter_id, nutrition_id, tstamp, description) VALUES (%s, %s, %s, %s, %s, %s) '''
+#				% ("14", "14", "9", "", "", ""))
+#
+#cursor.execute(''' INSERT INTO event_log (event_id, plant_id, planter_id, nutrition_id, tstamp, description) VALUES (%s, %s, %s, %s, %s, %s) '''
+#				% ("15", "15", "9", "", "", ""))
+#
+connection.commit()
+connection.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
