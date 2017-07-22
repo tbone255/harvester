3945786f33e23afada8f01c6ce6a003c87659a35
eli <elixircist@gmail.com>
2017-07-22 19:44:50 -0400

    angular bundle

--- harvester/webserver.py
+++ harvester/webserver.py
@@ -8,34 +8,7 @@ import time
 
 
 is_closing = False
-
-
-class MainHandler(tornado.web.RequestHandler):
-	def get(self):
-		#rows = dbutil get rows
-		self.render("index.html", entries = rows)
-
-class AddHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.render('add.html')
-
-	def post(self):
-		event = self.get_argument(name='event')
-		veg = self.get_argument(name='veg')
-		planter = int(self.get_argument(name='planter'))
-		timelog = time.strftime('%Y-%m-%d %H:%M:%S')
-        #dbutil add row (event veg planter timelog)
-		self.redirect("/add")
-
-
-
-def make_app():
-	return tornado.web.Application([
-		(r'/', MainHandler),
-		(r'/(Tomatoes.png)', tornado.web.StaticFileHandler, {'path':'./'}),
-		(r'/(custom.css)', tornado.web.StaticFileHandler, {'path': './'}),
-		(r'/add', AddHandler)
-	])
+app_path = '/Users/eli/Documents/harvester/harvester/harvester_fe/harvester-app/'
 
 #looks for a signal
 def signal_handler(signum, frame):
@@ -52,7 +25,23 @@ def exit():
 		logging.info('exited')
 
 
+class MainHandler(tornado.web.RequestHandler):
+	def get(self):
+		self.render(app_path + 'dist/index.html')
 
+def make_app():
+	return tornado.web.Application([
+		(r'/', MainHandler),
+		(r'/favicon.ico', tornado.web.StaticFileHandler, {'path':app_path + 'dist/favicon.ico'}),
+		(r'/inline.31a4c59f35c74ed381bc.bundle.js', tornado.web.StaticFileHandler, {'path':app_path + 'dist/inline.31a4c59f35c74ed381bc.bundle.js'}),
+		(r'/main.bc5a7e610b2682bd43f7.bundle.js', tornado.web.StaticFileHandler, {'path':app_path + 'main.bc5a7e610b2682bd43f7.bundle.js'}),
+		(r'/polyfills.ab8304790a25edec7f7d.bundle.js', tornado.web.StaticFileHandler, {'path':app_path + 'dist/polyfills.ab8304790a25edec7f7d.bundle.js'}),
+		(r'/styles.d41d8cd98f00b204e980.bundle.css', tornado.web.StaticFileHandler, {'path':app_path + 'dist/styles.d41d8cd98f00b204e980.bundle.css'}),
+		(r'/vendor.a3600978aa648c90bd39.bundle.js', tornado.web.StaticFileHandler, {'path':app_path + 'dist/vendor.a3600978aa648c90bd39.bundle.js'})
+		#(r'/(Tomatoes.png)', tornado.web.StaticFileHandler, {'path':'./'}),
+		#(r'/(custom.css)', tornado.web.StaticFileHandler, {'path': './'}),
+		#(r'/add', AddHandler)
+	])
 
 if __name__ == "__main__":
 	tornado.options.parse_command_line()
