06814145d09d7e34751fc9fc93310055de2a08f3
eli <elixircist@gmail.com>
2017-07-24 14:29:29 -0400

    angular

---
+++ harvester_fe/harvesterApp/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationBarComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
