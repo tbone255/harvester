e7bed72b21c48eb6e893c0eae5414cfdef826eb6
reza <reza@rezas-MBP.home>
2017-07-25 01:44:20 -0400

    Navbar stuff

--- harvester_fe/harvesterApp/src/app/app.module.ts
+++ harvester_fe/harvesterApp/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+
 @NgModule({
   declarations: [
     AppComponent, NavbarComponent
--- harvester_fe/harvesterApp/src/app/components/navbar/navbar.component.ts
+++ harvester_fe/harvesterApp/src/app/components/navbar/navbar.component.ts
@@ -3,10 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.less']
 })
 export class NavbarComponent implements OnInit {
-
+  private LOGO = require("./seedling.png");
   constructor() { }
 
   ngOnInit() {
