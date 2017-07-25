3fe87766cc6ced49943f6d6b71c97e2d50d2fcb8
eli <elixircist@gmail.com>
2017-07-25 08:55:49 -0400

    router links and separate components

--- harvester_fe/harvesterApp/src/app/app.module.ts
+++ harvester_fe/harvesterApp/src/app/app.module.ts
@@ -3,13 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { InventoryComponent } from './components/inventory/inventory.component';
+import { AboutComponent } from './components/about/about.component';
+import { AdminComponent } from './components/admin/admin.component';
+
+import { routes } from './app.router';
 
 @NgModule({
   declarations: [
-    AppComponent, NavbarComponent
+    AppComponent, NavbarComponent, HomeComponent, InventoryComponent, AboutComponent, AdminComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routes
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ harvester_fe/harvesterApp/src/app/app.router.ts
@@ -0,0 +1,18 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { InventoryComponent } from './components/inventory/inventory.component';
+import { AboutComponent } from './components/about/about.component';
+import { AdminComponent } from './components/admin/admin.component';
+
+export const router: Routes = [
+	{ path: '', redirectTo: 'home', pathMatch: 'full' },
+	{ path: 'home', component: HomeComponent },
+	{ path: 'inventory', component: InventoryComponent },
+	{ path: 'about', component: AboutComponent },
+	{ path: 'admin', component: AdminComponent }
+];
+
+export const routes: ModuleWithProviders = RouterModule.forRoot(router);
