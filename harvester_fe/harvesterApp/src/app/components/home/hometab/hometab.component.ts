282ca4324e753dc5b42087ab9834db791fc349b1
eli <elixircist@gmail.com>
2017-07-26 11:26:52 -0400

    angular stuff

---
+++ harvester_fe/harvesterApp/src/app/components/home/hometab/hometab.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hometab',
+  templateUrl: './hometab.component.html',
+  styleUrls: ['./hometab.component.css']
+})
+export class HometabComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
